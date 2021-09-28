8b62494b5c95582424edb801c5189807519081c6
Logan Wallis <Logan.Wallis@infotech.pri>
2021-09-28 11:29:41 -0600

    Visual Studio Files added

---
+++ CalcLibrary/CalculatorFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcLibrary
+{
+   public class CalculatorFunctions
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CalculatorCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CalcLibrary;
+namespace CalculatorCLI
+{
+    class Program
+    {
+        CalculatorFunctions calc = new CalculatorFunctions();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
