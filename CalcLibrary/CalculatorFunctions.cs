4866268c6129717406ee1a532f7badbf01a8b21d
Logan Wallis <Logan.Wallis@infotech.pri>
2021-10-01 11:21:38 -0600

    Started implementing commands for the COmmand line interface

---
+++ CalcLibrary/CalculatorFunctions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcLibrary
+{
+   public class CalculatorFunctions
+    {
+        //Addition
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public double add(int a, double b)
+        {
+            return a + b;
+        }
+        public double add(double a, int b)
+        {
+            return a + b;
+        }
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        //Subtraction
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public double sub(int a, double b)
+        {
+            return a - b;
+        }
+        public double sub(double a, int b)
+        {
+            return a - b;
+        }
+        public double sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        //Division
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
+        public double div(int a, double b)
+        {
+            return a / b;
+        }
+        public double div(double a, int b)
+        {
+            return a / b;
+        }
+        public double div(double a, double b)
+        {
+            return a / b;
+        }
+
+        //Multiplication
+        public int mult(int a, int b)
+        {
+            return a * b;
+        }
+        public double mult(int a, double b)
+        {
+            return a * b;
+        }
+        public double mult(double a, int b)
+        {
+            return a * b;
+        }
+        public double mult(double a, double b)
+        {
+            return a * b;
+        }
+
+       public int dec2hex(int a)
+        {
+            a.ToString("X");
+            return a;
+        }
+
+        public string hex2dec(string a)
+        {
+            Convert.ToInt32(a, 16).ToString();
+            return a;
+        }
+
+
+
+    }
+}
---
+++ CalculatorCLI/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using CalcLibrary;
+namespace CalculatorCLI
+{
+    class Program
+    {
+
+        static object num1;
+        static object num2;
+
+        static void Main(string[] args)
+        {
+            CalculatorFunctions calc = new CalculatorFunctions();
+
+            try { num1 = double.Parse(args[1]); } catch { }
+            try { num2 = double.Parse(args[2]); } catch { }
+
+            try { num1 = int.Parse(args[1]); } catch { }
+            try { num2 = int.Parse(args[2]); } catch { }
+
+            Console.WriteLine("/?");
+
+                switch (args[0])
+                {
+                    case "/add":
+                        if (num1 is double && num2 is double)
+                           Console.WriteLine(calc.add((double)num1, (double)num2));
+                        else if (num1 is double && num2 is int)
+                           Console.WriteLine(calc.add((double)num1, (int)num2));
+                        else if (num1 is int && num2 is double)
+                           Console.WriteLine(calc.add((int)num1, (double)num2));
+                        else if (num1 is int && num2 is int)
+                           Console.WriteLine(calc.add((int)num1, (int)num2));
+                        break;
+
+                    case "/sub":
+                        if (num1 is double && num2 is double)
+                            Console.WriteLine(calc.sub((double)num1, (double)num2));
+                        else if (num1 is double && num2 is int)
+                            Console.WriteLine(calc.sub((double)num1, (int)num2));
+                        else if (num1 is int && num2 is double)
+                            Console.WriteLine(calc.sub((int)num1, (double)num2));
+                        else if (num1 is int && num2 is int)
+                            Console.WriteLine(calc.sub((int)num1, (int)num2));
+                        break;
+
+                    case "/div":
+                        if (num1 is double && num2 is double)
+                            Console.WriteLine(calc.div((double)num1, (double)num2));
+                        else if (num1 is double && num2 is int)
+                            Console.WriteLine(calc.div((double)num1, (int)num2));
+                        else if (num1 is int && num2 is double)
+                            Console.WriteLine(calc.div((int)num1, (double)num2));
+                        else if (num1 is int && num2 is int)
+                            Console.WriteLine(calc.div((int)num1, (int)num2));
+                        break;
+
+                    case "/mult":
+                        if (num1 is double && num2 is double)
+                            Console.WriteLine(calc.mult((double)num1, (double)num2));
+                        else if (num1 is double && num2 is int)
+                            Console.WriteLine(calc.mult((double)num1, (int)num2));
+                        else if (num1 is int && num2 is double)
+                            Console.WriteLine(calc.mult((int)num1, (double)num2));
+                        else if (num1 is int && num2 is int)
+                            Console.WriteLine(calc.mult((int)num1, (int)num2));
+                        break;
+
+                    case "/dec2hex":
+                       Console.WriteLine(calc.dec2hex(int.Parse(args[1])));
+                        break;
+
+                    case "/hex2dec":
+                       Console.WriteLine(calc.hex2dec(args[1]));
+                        break;
+
+                    case "/?":
+                        Console.WriteLine(" The following commands you can use are: /add | /sub | /div | /mult | /dec2hex | /hex2dec");
+                        break;
+                }
+            
+
+        }
+    }
+}
